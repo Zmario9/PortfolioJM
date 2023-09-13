14fde8f5abb12ea6ae74b47e50cf04de941c37b6
Jose Manuel <jcorreacastro99@gmail.com>
2023-09-13 14:00:17 -0400

    Funciona MDB aun habiendo agregado codigo de Typewriter

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import Typewriter from 't-writer.js';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  ngOnInit():void {
+    const target = document.querySelector('#owner');
+    const writeOwner1 = new Typewriter(target,{
+      typeColor: 'red',
+    })
 
+    writeOwner1
+    .type('Magus Developer')
+    .start();
+  }
 }
